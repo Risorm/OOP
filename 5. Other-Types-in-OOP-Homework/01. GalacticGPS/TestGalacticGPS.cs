8a567a8d5f27ea4c0addfeba11d448e434e9a6ef
krastoss <servicemail945@gmail.com>
2015-02-09 17:40:08 +0200

    HW Other types started

---
+++ 5. Other-Types-in-OOP-Homework/01. GalacticGPS/TestGalacticGPS.cs
@@ -0,0 +1,11 @@
+﻿namespace _01.GalacticGPS
+{
+    public static class TestGalacticGps
+    {
+        static void Main()
+        {
+            Location home = new Location(18.037986, 28.870097, Planet.Earth);
+            System.Console.WriteLine(home);
+        }
+    }
+}
---
+++ 5. Other-Types-in-OOP-Homework/02. Fraction Calculator/Fraction.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Fraction_Calculator
+{
+    struct Fraction
+    {
+        private int denominator;
+
+        public Fraction(int denominator, int numerator) : this()
+        {
+            this.Denominator = denominator;
+            Numerator = numerator;
+        }
+
+        public int Numerator { get; set; }
+
+        public int Denominator
+        {
+            get { return this.denominator; }
+            set
+            {
+                if (value == 0)
+                {
+                    throw new ArgumentOutOfRangeException("Value cannot be less than 0!");
+                }
+                this.denominator = value;
+            }
+        }
+
+        public static Fraction operator +(Fraction a, Fraction b)
+        {
+
+            Fraction fraction = new Fraction();
+            fraction.denominator = 
+        }
+    }
+}
